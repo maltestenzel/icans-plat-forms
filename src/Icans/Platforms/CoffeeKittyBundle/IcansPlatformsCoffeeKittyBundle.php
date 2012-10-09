bfe1563dceaa5fb8ea2ff77e68ea16fa004e16eb
malte.stenzel <malte.stenzel@icans-gmbh.com>
2012-10-09 13:32:54 +0200

    Adding comments for generated coffeekittybundle file

--- src/Icans/Platforms/CoffeeKittyBundle/IcansPlatformsCoffeeKittyBundle.php
+++ src/Icans/Platforms/CoffeeKittyBundle/IcansPlatformsCoffeeKittyBundle.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Declares the DefaultController
+ *
+ * origin: GM
+ *
+ * @author    Malte Stenzel
+ * @copyright 2007 - 2012 ICANS GmbH
+ */
 namespace Icans\Platforms\CoffeeKittyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
