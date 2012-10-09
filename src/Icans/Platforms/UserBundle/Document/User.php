0a1c5d6169f1e0893c356abdcff45f95ef283d10
plat.forms <plat.forms@icans-gmbh.com>
2012-10-09 05:39:53 -0700

    added form extensions for the user to include the statistics added language variable for form moved document to model

--- src/Icans/Platforms/UserBundle/Document/User.php
+++ src/Icans/Platforms/UserBundle/Document/User.php
@@ -11,83 +11,14 @@
 
 namespace Icans\Platforms\UserBundle\Document;
 
-use FOS\UserBundle\Document\User as BaseUser;
+use Icans\Platforms\UserBundle\Model\User as UserModel;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
- * Document extending the default FOS User with the attributes needed for the CafMan Application.
+ * Class to be used as a MongoDB document.
  *
  * @MongoDB\Document
- *
- * @codeCoverageIgnore Document class with getter and setters.
  */
-class User extends BaseUser
+class User extends UserModel
 {
-    /**
-     * @MongoDB\Id(strategy="auto")
-     */
-    protected $id;
-
-    /**
-     * @MongoDb\String
-     * @var string|null
-     */
-    protected $defaultKittyId = null;
-
-    /**
-     * @MongoDb\Boolean
-     * @var boolean
-     */
-    protected $statisticPublic = false;
-
-    /**
-     * Returns the id of the user.
-     *
-     * @return string
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Returns the users default kitty identifier.
-     *
-     * @return string
-     */
-    public function getDefaultKittyId()
-    {
-        return $this->defaultKittyId;
-    }
-
-    /**
-     * Set the users default kitty identifier.
-     *
-     * @param string $defaultKittyId
-     */
-    public function setDefaultKittyId($defaultKittyId)
-    {
-        $this->defaultKittyId = $defaultKittyId;
-    }
-
-    /**
-     * Returns whether the statistic of the user is publically visible.
-     *
-     * @return boolean
-     */
-    public function isStatisticPublic()
-    {
-        return $this->statisticPublic;
-    }
-
-    /**
-     * Sets whether the statistic of the user is publically visible.
-     *
-     * @param boolean $isStatisticPublic
-     */
-    public function setStatisticPublic($isStatisticPublic)
-    {
-        $this->statisticPublic = $isStatisticPublic;
-    }
-
 }
---
+++ src/Icans/Platforms/UserBundle/Model/User.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Declares User
+ *
+ * origin: GM
+ *
+ * @author    Thorsten 'stepo' Hallwas
+ * @author    Malte Stenzel
+ * @copyright ${date} ICANS GmbH
+ */
+
+namespace Icans\Platforms\UserBundle\Model;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * Document extending the default FOS User with the attributes needed for the CafMan Application.
+ *
+ * @codeCoverageIgnore Model class with getter and setters.
+ */
+class User extends BaseUser
+{
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDb\String
+     * @var string|null
+     */
+    protected $defaultKittyId = null;
+
+    /**
+     * @MongoDb\Boolean
+     * @var boolean
+     */
+    protected $statisticPublic = false;
+
+    /**
+     * Returns the users default kitty identifier.
+     *
+     * @return string
+     */
+    public function getDefaultKittyId()
+    {
+        return $this->defaultKittyId;
+    }
+
+    /**
+     * Set the users default kitty identifier.
+     *
+     * @param string $defaultKittyId
+     *
+     * @return User
+     */
+    public function setDefaultKittyId($defaultKittyId)
+    {
+        $this->defaultKittyId = $defaultKittyId;
+
+        return $this;
+    }
+
+    /**
+     * Returns whether the statistic of the user is publically visible.
+     *
+     * @return boolean
+     */
+    public function isStatisticPublic()
+    {
+        return $this->statisticPublic;
+    }
+
+    /**
+     * Sets whether the statistic of the user is publically visible.
+     *
+     * @param boolean $isStatisticPublic
+     *
+     * @return User
+     */
+    public function setStatisticPublic($isStatisticPublic)
+    {
+        $this->statisticPublic = $isStatisticPublic;
+        
+        return $this;
+    }
+}
