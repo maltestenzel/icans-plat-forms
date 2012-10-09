c35fb3fc9e36476f094eb54d9a5ae4b57d88e38c
plat.forms <plat.forms@icans-gmbh.com>
2012-10-09 10:54:31 -0700

    set fosuserbundle as parent for userbundle

--- src/Icans/Platforms/UserBundle/IcansPlatformsUserBundle.php
+++ src/Icans/Platforms/UserBundle/IcansPlatformsUserBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IcansPlatformsUserBundle extends Bundle
 {
+    /**
+     * Define the FOSUserBundle as parent to be able to overwrite.
+     *
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
