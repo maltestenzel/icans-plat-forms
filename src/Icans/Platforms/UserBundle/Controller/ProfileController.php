99a31514c8ced2d864bf4b3205cc2bb63b0e7392
plat.forms <plat.forms@icans-gmbh.com>
2012-10-09 11:34:09 -0700

    added action and template for defaultkitty

--- src/Icans/Platforms/UserBundle/Controller/ProfileController.php
+++ src/Icans/Platforms/UserBundle/Controller/ProfileController.php
@@ -16,6 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Icans\Platforms\UserBundle\Form\Type\DefaultKittyFormType;
 
 /**
  * Controller handling the profile display and editing functionality for users.
@@ -64,4 +65,44 @@ class ProfileController extends Controller
             'form' => $profileForm->createView(),
         );
     }
+
+    /**
+     * Displays the form to edit the defaultkitty.
+     *
+     * @Route("/profile/{username}/defaultkitty/", name="user_profile_defaultkitty")
+     * @Template()
+     */
+    public function defaultkittyAction()
+    {
+        // reenable to secure
+        /*$user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }*/
+
+        // @todo replace with service
+        $kitties = array();
+        $defaultKitty = null;
+
+
+        $defaultKittyType = new DefaultKittyFormType(
+            'Icans\Platforms\UserBundle\Document\User',
+            $kitties,
+            $defaultKitty
+        );
+
+        $profile = new User();
+        $formFactory = $this->container->get('form.factory');
+
+        $defaultkittyForm = $formFactory->create(
+            $defaultKittyType,
+            $profile,
+            array()
+        );
+
+        return array(
+            'form' => $defaultkittyForm->createView(),
+        );
+    }
+
 }
--- src/Icans/Platforms/UserBundle/Form/Type/DefaultKittyFormType.php
+++ src/Icans/Platforms/UserBundle/Form/Type/DefaultKittyFormType.php
@@ -30,20 +30,20 @@ class DefaultKittyFormType extends AbstractType
     /**
      * @var string|null
      */
-    protected $defaultKitty;
+    protected $defaultKittyId;
 
     /**
      * Constructor.
      *
-     * @param string      $dataClass    class holding the user data
-     * @param array       $userKitties  array of kities the user can choose his default from.
-     * @param string|null $defaultKitty the id of the current default kitty
+     * @param string      $dataClass      class holding the user data
+     * @param array       $userKitties    array of kities the user can choose his default from.
+     * @param string|null $defaultKittyId the id of the current default kitty
      */
-    public function __construct($dataClass, array $userKitties, $defaultKitty)
+    public function __construct($dataClass, array $userKitties, $defaultKittyId)
     {
         $this->dataClass    = $dataClass;
         $this->userKitties  = $userKitties;
-        $this->defaultKitty = $defaultKitty;
+        $this->defaultKittyId = $defaultKittyId;
     }
 
     /**
@@ -51,23 +51,24 @@ class DefaultKittyFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // we only display this form if the user has already kitties to choose from.
+        $kitties = array();
         if (count($this->userKitties) > 0) {
-            $defaultKittyOptions = array(
-                'choices' => $this->userKitties,
-                'label' => 'form.selectkitty',
-                'required' => false
-            );
-            if (null !== $this->defaultKitty) {
-                $defaultKittyOptions['prefered_choices'] = array($this->defaultKitty);
-            }
-            $builder
-                ->add(
-                    'defaultKitty',
-                    'choice',
-                    $defaultKittyOptions
-                );
+            $kitties = $this->userKitties;
         }
+        $defaultKittyOptions = array(
+            'choices' => $kitties,
+            'label' => 'form.selectkitty',
+            'required' => false
+        );
+        if (null !== $this->defaultKittyId) {
+            $defaultKittyOptions['prefered_choices'] = array($this->defaultKittyId);
+        }
+        $builder
+            ->add(
+                'defaultKittyId',
+                'choice',
+                $defaultKittyOptions
+            );
     }
 
     /**
