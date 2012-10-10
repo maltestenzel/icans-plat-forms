3382a2b97458951e3e83cb2b1255dfb70f8c1c45
malte.stenzel <malte.stenzel@icans-gmbh.com>
2012-10-10 12:24:57 +0200

    Factory reset implemented, needs styling

--- src/Icans/Platforms/CafManBundle/Controller/AdminController.php
+++ src/Icans/Platforms/CafManBundle/Controller/AdminController.php
@@ -9,13 +9,15 @@
  */
 namespace Icans\Platforms\CafManBundle\Controller;
 
-use Icans\Platforms\CafManBundle\Api\MultiFormServiceInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Implements a controller containing admin functionality, i.e. reset the application
@@ -30,8 +32,30 @@ class AdminController extends Controller
      *
      * @Template()
      */
-    public function resetAction()
+    public function resetAction(Request $request)
     {
+        if ($request->isMethod('POST')) {
+            // Reset the application
+            /* @var $kernel KernelInterface */
+            $kernel = $this->get('kernel');
+            $application = new Application($kernel);
+            $application->setAutoExit(false);
+
+            // Drop dbs
+            $options = array('command' => 'doctrine:mongodb:schema:drop');
+            $application->run(new ArrayInput($options));
+            $options = array('command' => 'doctrine:mongodb:schema:create');
+            $application->run(new ArrayInput($options));
+
+            // Create admin user
+            $options = array('command' => 'fos:user:create', 'username' => 'admin', 'email' => 'organizers@plat-forms.org', 'password' => 'admin');
+            $application->run(new ArrayInput($options));
+            $options = array('command' => 'fos:user:promote', 'username' => 'admin', '--super' => true);
+            $application->run(new ArrayInput($options));
+
+            return new RedirectResponse($this->generateUrl('fos_user_security_logout'));
+        }
+
         return array();
     }
 }
