7e1cba7adb3eb9293538237eb290c9c8f0cde68a
plat.forms <plat.forms@icans-gmbh.com>
2012-10-10 00:08:03 -0700

    new method for declining the koffekitty for a user and new method to add balance

--- src/Icans/Platforms/CoffeeKittyBundle/Api/KittyUserServiceInterface.php
+++ src/Icans/Platforms/CoffeeKittyBundle/Api/KittyUserServiceInterface.php
@@ -22,4 +22,43 @@ use Icans\Platforms\UserBundle\Api\UserInterface;
  */
 interface KittyUserServiceInterface
 {
+    /**
+     * Declines the membership for the user with the given kitty.
+     *
+     * @param KittyInterface $kitty
+     * @param UserInterface $user
+     *
+     * @throws NotFoundException In case the kitty to user relation document cannot be found.
+     */
+    public function declineMembership(KittyInterface $kitty, UserInterface $user);
+
+    /**
+     * Accpets the membership for the user with the given kitty.
+     *
+     * @param KittyInterface $kitty
+     * @param UserInterface $user
+     *
+     * @throws NotFoundException In case the kitty to user relation document cannot be found.
+     */
+    public function acknowledgeMembership(KittyInterface $kitty, UserInterface $user);
+
+    /**
+     * Requests the membership for the user with the given kitty.
+     *
+     * @param KittyInterface $kitty
+     * @param UserInterface $user
+     *
+     * @throws AlreadyExistsException
+     */
+    public function requestMembership(KittyInterface $kitty, UserInterface $user);
+
+    /**
+     * Adds the given balance to the given kitty for the given user.
+     *
+     * @param KittyInterface $kitty
+     * @param UserInterface $user
+     *
+     * @throws NotFoundException In case the kitty to user relation document cannot be found.
+     */
+    public function addBalance(KittyInterface $kitty, UserInterface $user, $balance);
 }
